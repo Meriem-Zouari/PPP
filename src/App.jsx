1fce8e3c90b16aa0150d41001171a2e1717a5b75
ines-el-bour <ines.elbour@insat.ucar.tn>
2024-04-18 21:34:04 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+{/*import { useState } from 'react'*/}
+
+import './App.css'
+
+// App.jsx
+
+import React from 'react';
+import Booking from './Booking';
+
+function App() {
+  return (
+    <div>
+      <Booking/>
+    </div>
+  );
+}
+
+export default App;
