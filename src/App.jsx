9a5ba2f3bb77b70bfc809f5b61c8a68dadf8f854
ines-el-bour <ines.elbour@insat.ucar.tn>
2024-05-29 21:52:34 +0100

    routing pages Connect and Booking

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,21 @@ import './App.css'
 // App.jsx
 
 import React from 'react';
-import Booking from './Booking';
+import Booking from './Pages/Booking/Booking';
+import Connect from './Pages/Connect/Connect';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Booking/>
-    </div>
+    <>
+    <Router>
+      <Routes>
+      <Route path="/Connect" element={<Connect/>}/>
+      <Route path="/Booking" element={<Booking/>}/>
+      
+      </Routes>
+      </Router>
+    </>
   );
 }
 
--- src/Pages/Connect/Connect.jsx
+++ src/Pages/Connect/Connect.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 import './Connect.css'
 
-import user_icon from '../../resources/person.png'
-import email_icon from '../../resources/email.png'
-import password_icon from '../../resources/password.png'
-import backgroundImage from '../../resources/BACKGROUND.jpg'; // Import your image
+import user_icon from '../../assets/person.png'
+import email_icon from '../../assets/email.png'
+import password_icon from '../../assets/password.png'
+import backgroundImage from '../../assets/BACKGROUND.jpg'; // Import your image
 
 const LoginSignup = () => {
   const [action,setAction]=useState("Sign Up")
