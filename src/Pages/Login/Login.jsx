a12a9ca87c41dd16f72b986376e9f1171f36f743
meriam <meriam.zouari@insat.ucar.tn>
2024-06-02 19:03:46 +0100

    back

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,37 @@
 import './Login.css'
+import { useState } from 'react'
 import email_icon from '../../assets/email.png'
 import password_icon from '../../assets/password.png'
 import backgroundImage from '../../assets/Booking.jpg'; 
+import httpClient from '../httpClient'
 
 const Login = () => {
+
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+
+  const logInUser = async ()=>{
+    console.log(email,password);
+
+    try{
+      const resp = await httpClient.post("http://localhost:5000/Login",{
+        email,
+        password,
+      });
+
+      window.location.href = "/";
+    } catch (error){
+      if(error.response.status === 401){
+        alert("Invalid credentials")
+      }
+    }
+
+
+
+  };
+
+
+
   return (
     <div className="background"
     style={{
@@ -23,15 +51,15 @@ const Login = () => {
 
           <div className="input">
             <img src={email_icon} alt="" />
-            <input type="email" placeholder='Email Id' />
+            <input type="email" placeholder='Email Id' value={email} onChange={(e) => setEmail(e.target.value)} />
           </div>
           <div className="input">
             <img src={password_icon} alt="" />
-            <input type="password" placeholder='Password' />
+            <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
           </div>
         </div>
         <div className="submit-container">
-          <button className="submit" type="button"  >
+          <button className="submit" type="button" onClick={() => logInUser()} >
             Sign in
           </button>
         </div> 
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,10 +1,40 @@
 import './Register.css'
+import { useState } from 'react'
+
 import user_icon from '../../assets/person.png'
 import email_icon from '../../assets/email.png'
 import password_icon from '../../assets/password.png'
 import backgroundImage from '../../assets/Booking.jpg'; 
+import httpClient from '../httpClient'
+
+
 
 const Register = () => {
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+  const [userName,setuserName] = useState("")
+
+
+  const registerUser = async ()=>{
+    console.log(email,password);
+
+    try{
+      const resp = await httpClient.post("//localhost:5000/Register",{
+        userName,
+        email,
+        password,
+      });
+
+      window.location.href = "/";
+    } catch (error){
+      if(error.response.status === 401){
+        alert("Invalid credentials")
+      }
+    }
+
+
+
+  };
   return (
     <div className="background"
     style={{
@@ -23,20 +53,20 @@ const Register = () => {
         <div className="inputs">
           <div className="input">
             <img src={user_icon} alt="" x/>
-            <input type="text" placeholder='UserName' />
+            <input type="text" placeholder='UserName' value={userName} onChange={(e) => setuserName(e.target.value)} />
           </div>
 
           <div className="input">
             <img src={email_icon} alt="" />
-            <input type="email" placeholder='Email Id' />
+            <input type="email" placeholder='Email Id' value={email} onChange={(e) => setEmail(e.target.value)}/>
           </div>
           <div className="input">
             <img src={password_icon} alt="" />
-            <input type="password" placeholder='Password' />
+            <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
           </div>
         </div>
         <div className="submit-container">
-          <button className="submit" type="button"  >
+          <button className="submit" type="button" onClick={() => registerUser()} >
             Sign up
           </button>
         </div> 
